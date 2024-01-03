e6d8429588868286e70e0d384103710afdedea24
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-03 10:56:05 -0600

    Const for the win!

---
+++ ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+// cpmoore0317
+
+export default function taskBlock(trueOrFalse) {
+    const task = false;
+    const task2 = true;
+  
+    if (trueOrFalse) {
+      var task = true;
+      var task2 = false;
+    }
+  
+    return [task, task2];
+  }
