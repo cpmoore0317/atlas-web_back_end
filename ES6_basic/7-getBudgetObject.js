5f783d29595911757ea1feca43bfa4b8f977c5ad
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-04 10:40:50 -0600

    Lint pass for task 7

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,11 +1,11 @@
 // cpmoore0317
 
 export default function getBudgetObject(income, gdp, capita) {
-    const budget = {
-      income,
-      gdp,
-      capita,
-    };
-  
-    return budget;
-  }
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
