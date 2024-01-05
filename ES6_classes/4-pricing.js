422a06dc9839ef08ca85c0e743a4d64c631cec71
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-05 15:03:25 -0600

    Correct ESLint formatting for 4-pricing.js

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,40 @@
+// cpmoore0317
+
+import Currency from "./3-currency.js";
+
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  // Setter for amount
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+
+  // Getter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  // Setter for currency
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  // Method to display full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  // Static method to convert price
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
