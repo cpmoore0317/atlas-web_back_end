c219bab8a21d6111f218b6360dcc9df3d4bc6527
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-05 15:44:53 -0600

    Complete class setup for Airport

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,23 @@
+// cpmoore0317
+
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  // Getter for name
+  get name() {
+    return this._name;
+  }
+
+  // Getter for code
+  get code() {
+    return this._code;
+  }
+
+  // Override the default toString method
+  toString() {
+    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+  }
+}
