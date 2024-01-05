a63faf880b37bd15d85c5704187a3dfa71347c6e
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-05 16:19:17 -0600

    Adding task 8 valueof and tostring methods

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,28 @@
+// cpmoore0317
+
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = size;
+    this._location = location;
+  }
+
+  // Getter for size
+  get size() {
+    return this._size;
+  }
+
+  // Getter for location
+  get location() {
+    return this._location;
+  }
+
+  // Override the valueOf method for number casting
+  valueOf() {
+    return this._size;
+  }
+
+  // Override the toString method for string casting
+  toString() {
+    return this._location;
+  }
+}
