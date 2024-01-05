78c8b7bf867feffc193b1212dfb60cfd1e629420
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-05 12:28:23 -0600

    Using new dictionary for appending in task 10

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,9 +1,10 @@
 // cpmoore0317
 
 export default function appendToEachArrayValue(array, appendString) {
-  for (let value of array) {
-    value = appendString + value;
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
 
-  return array;
+  return newArray;
 }
