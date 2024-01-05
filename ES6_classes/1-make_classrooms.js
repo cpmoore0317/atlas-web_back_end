fda36c669d4f987795ba45c0332525f8fbd8c401
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-05 13:05:53 -0600

    Initialize rooms testing

---
+++ ES6_classes/1-make_classrooms.js
@@ -0,0 +1,11 @@
+// cpmoore0317
+
+import ClassRoom from './0-ClassRoom.js';
+
+export default function initializeRooms() {
+  const room1 = new ClassRoom(19);
+  const room2 = new ClassRoom(20);
+  const room3 = new ClassRoom(34);
+
+  return [room1, room2, room3];
+}
