22f2748794f2dbbbf44185f664d5c1a3db1091dd
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-05 15:26:04 -0600

    Testing for task 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -2,16 +2,20 @@
 
 export default class Building {
   constructor(sqft) {
+    if (
+      this.constructor !== Building &&
+      typeof this.evacuationWarningMessage !== "function"
+    ) {
+      throw new Error(
+        "Class extending Building must override evacuationWarningMessage"
+      );
+    }
+
     this._sqft = sqft;
   }
 
   // Getter for sqft
   get sqft() {
     return this._sqft;
-  }
-
-  // Abstract method to be implemented by subclasses
-  evacuationWarningMessage() {
-    throw new Error("Class extending Building must override evacuationWarningMessage");
   }
 }
