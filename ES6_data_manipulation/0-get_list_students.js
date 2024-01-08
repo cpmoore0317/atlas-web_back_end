0470b4fffe643485938e13a96814bbab5909acea
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-08 14:27:24 -0600

    Retrieving students list function

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,22 @@
+// cpmoore0317
+
+export default function getListStudents() {
+  const students = [];
+  const guillaume = {
+    id: 1,
+    firstName: "Guillaume",
+    location: "San Francisco",
+  };
+  const james = {
+    id: 2,
+    firstName: "James",
+    location: "Columbia",
+  };
+  const serena = {
+    id: 5,
+    firstName: "Serena",
+    location: "San Francisco",
+  };
+  students.push(guillaume, james, serena);
+  return students;
+}
