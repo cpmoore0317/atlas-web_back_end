a07c20c381da5a61279cc5551138c9202359d298
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-08 14:30:26 -0600

    Fixing string quotes and end of line sequence for task 0

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -4,18 +4,18 @@ export default function getListStudents() {
   const students = [];
   const guillaume = {
     id: 1,
-    firstName: "Guillaume",
-    location: "San Francisco",
+    firstName: 'Guillaume',
+    location: 'San Francisco',
   };
   const james = {
     id: 2,
-    firstName: "James",
-    location: "Columbia",
+    firstName: 'James',
+    location: 'Columbia',
   };
   const serena = {
     id: 5,
-    firstName: "Serena",
-    location: "San Francisco",
+    firstName: 'Serena',
+    location: 'San Francisco',
   };
   students.push(guillaume, james, serena);
   return students;
