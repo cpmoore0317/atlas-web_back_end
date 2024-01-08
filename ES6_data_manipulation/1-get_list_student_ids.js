2db2b48fb7511cd8cc79ccc06055d96d12df577c
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-08 14:39:58 -0600

    Mapping students id attribute to a new array

---
+++ ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,11 @@
+// cpmoore0317
+
+export default function getListStudentIds(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
+
+  const ids = students.map((student) => student.id);
+
+  return ids;
+}
