373a858f968952a4a49f5da4c5d70509851af5f2
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-09 09:57:28 -0600

    Adding trailing comma for Lint task 2

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,7 +2,7 @@
 
 export default function getStudentsByLocation(students, city) {
   const locationBasedList = students.filter(
-    (student) => student.location === city
+    (student) => student.location === city,
   );
   return locationBasedList;
 }
