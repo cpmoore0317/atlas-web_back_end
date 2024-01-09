443b32840e4fc0135555abff076ac575fba223b8
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-09 11:37:04 -0600

    Testing seperate variables for mapping and filter

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,19 +1,20 @@
 // cpmoore0317
 
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const updatedStudentGrades = students
-    .filter((student) => students.location === city)
-    .map((student) => {
-      const matchingGrade = newGrades.find(
-        (grade) => grade.studentId === student.id
-      );
-      const updatedGrade = matchingGrade ? matchingGrade.grade : "N/A";
-      return {
-        id: student.id,
-        firstName: student.firstName,
-        location: student.city,
-        grade: updatedGrade,
-      };
-    });
-  return updatedStudentGrades;
+  const studentsInCity = students.filter(
+    (student) => students.location === city
+  );
+  const updatedStudents = studentsInCity.map((student) => {
+    const matchingGrade = newGrades.find(
+      (grade) => grade.studentId === student.id
+    );
+    const updatedGrade = matchingGrade ? matchingGrade.grade : "N/A";
+    return {
+      id: student.id,
+      firstName: student.firstName,
+      location: student.city,
+      grade: updatedGrade,
+    };
+  });
+  return updatedStudents;
 }
