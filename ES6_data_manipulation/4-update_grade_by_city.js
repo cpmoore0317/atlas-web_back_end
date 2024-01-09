90dd983cd8d57261a666522e8807e843e0135a0b
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-09 11:26:14 -0600

    Testing for update grades by city

---
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,19 @@
+// cpmoore0317
+
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  const updatedStudentGrades = students
+    .filter((student) => students.location === city)
+    .map((student) => {
+      const matchingGrade = newGrades.find(
+        (grade) => grade.studentId === student.id
+      );
+    });
+  const updatedGrade = matchingGrade ? matchingGrade.grade : "N/A";
+  return {
+    id: student.id,
+    firstName: student.firstName,
+    location: student.city,
+    grade: updatedGrade,
+  };
+  return updatedStudentGrades;
+}
