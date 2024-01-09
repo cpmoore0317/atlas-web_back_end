07c64d6b6992d200f69aab27aa3e589286fe734e
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-09 11:30:11 -0600

    Testing return object placement within update function

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -7,12 +7,13 @@ export default function updateStudentGradeByCity(students, city, newGrades) {
       const matchingGrade = newGrades.find(
         (grade) => grade.studentId === student.id
       );
+      const updatedGrade = matchingGrade ? matchingGrade.grade : "N/A";
+      return {
+        id: student.id,
+        firstName: student.firstName,
+        location: student.city,
+        grade: updatedGrade,
+      };
     });
-  const updatedGrade = matchingGrade ? matchingGrade.grade : "N/A";
-  return {
-    id: student.id,
-    firstName: student.firstName,
-    location: student.city,
-    grade: updatedGrade,
-  };
+  return updatedStudentGrades;
 }
