467f40a964f06c0b0bbfb1c213dc9c7abd28cfeb
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-09 14:26:54 -0600

    ESLint fixes final for tak 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,7 @@
 // cpmoore0317
 
 export default function cleanSet(set, startString) {
-  const filteredValues = [...set].filter((value) =>
-    value.startsWith(startString),
-  );
+  const filteredValues = [...set].filter((value) => value.startsWith(startString));
   const resultString = filteredValues.join(' ,');
   return resultString;
 }
