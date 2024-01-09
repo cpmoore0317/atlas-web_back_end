30fc4e221fe1228578b8ed2b32756eb2e1e1ba33
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-09 14:19:28 -0600

    Using starts with to filter the correct sets, and using join to craete the string

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+// cpmoore0317
+
+export default function cleanSet(set, startString) {
+  const filteredValues = [...set].filter((value) =>
+    value.startsWith(startString)
+  );
+  const resultString = filteredValues.join(" ,");
+  return resultString;
+}
