1d3f2f4c3af8a2be5e894b65dff2184906e3bac2
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-10 14:48:37 -0600

    Removing unneccessary log for results

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,16 +1,17 @@
 // cpmoore0317
 
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((results) => {
-      const [photoResult, userResult] = results;
-      console.log(
-        `${photoResult.body} ${userResult.firstName} ${userResult.lastName}`
-      );
-    })
-    .catch((error) => {
+  return Promise.all([
+    uploadPhoto().catch((error) => {
       console.error('Signup system offline', error.message);
-    });
+    }),
+    createUser().catch((error) => {
+      console.error('Signup system offline', error.message);
+    }),
+  ])
+  .then((results) => {
+    const [photoResult, userResult] = results;
+  });
 }
