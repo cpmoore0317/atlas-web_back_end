edbdc54ceefa2f287997d186acf9a0defd4a58bf
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-10 15:30:09 -0600

    Reverting to Promise.all

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -4,15 +4,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  // Use Promise.allSettled to handle the settlement of promises
-  return Promise.allSettled([
+  return Promise.all([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
   ]).then((results) => {
-    // Map the results to an array with the desired structure
     return results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
+      status: result.status, // Status of the promise ('fulfilled' or 'rejected')
+      value: result.value, // Value or error returned by the promise
     }));
   });
 }
