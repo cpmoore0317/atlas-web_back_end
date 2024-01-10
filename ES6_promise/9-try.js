a5833c2ffce77d176e6db7823693219821f0a88b
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-10 15:52:56 -0600

    Finally! Seriously, useing finally to go through full guardrail process

---
+++ ES6_promise/9-try.js
@@ -0,0 +1,16 @@
+// cpmoore0317
+
+export default function guardrail(mathFuction) {
+  const queue = [];
+
+  try {
+    const result = mathFuction();
+    queue.push(result);
+  } catch (error) {
+    queue.push(error.toString());
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+
+  return queue;
+}
