c379c43c7f98d138ea88a526cef5371080d0ae14
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-01-10 15:02:46 -0600

    Testing another disable method for ESLint

--- ES6_promise/4-user-promise.js
+++ ES6_promise/4-user-promise.js
@@ -1,9 +1,10 @@
 // cpmoore0317
-/* eslint-disable-next-line object-shorthand */
 
 export default function signUpUser(firstName, lastName) {
   return Promise.resolve({
+    // eslint-disable-next-line object-shorthand
     firstName: firstName,
+    // eslint-disable-next-line object-shorthand
     lastName: lastName,
   });
 }
