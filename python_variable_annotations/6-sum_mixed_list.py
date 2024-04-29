d08df9d7f142ceff8df86618c9fc7ab42ea6736a
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-04-29 13:26:04 -0500

    Correcting syntax for mixed type list

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -5,7 +5,7 @@ Module for returning the sum of a list with mied types.
 from typing import List
 
 
-def sum_mixed_list(mxd_list: List[int, float]) -> float:
+def sum_mixed_list(mxd_list: List[int | float]) -> float:
     """
     Type-annotated function which takes a list of integers and floats
     and returns their sum as a float.
