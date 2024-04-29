d3700f2d2b80826e99397acb613422ee45b75427
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-04-29 12:32:49 -0500

    Simple version of str concat with type annotations

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Module for concatenating two strings.
+"""
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Type-annoted function that takes two strings
+    and returns a concatenated string.
+    """
+    return str1 + str2
