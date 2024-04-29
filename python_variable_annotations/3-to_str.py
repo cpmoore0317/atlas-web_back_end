dce6a3c153cbe93bc9b1388a855b9d2da87bca5b
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-04-29 12:51:03 -0500

    Adding whitespace after function colon

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -4,7 +4,7 @@ Module for returning a float as a string.
 """
 
 
-def to_str(n:float) -> str:
+def to_str(n:float) -> str: 
     """
     Type-annotated function that takes a float and
     returns the string representation of it.
