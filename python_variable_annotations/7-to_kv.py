17ca14f0684a83fd61d3369efa66aa336fb33408
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-04-29 13:43:25 -0500

    Specifying types for the Tuple return

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -5,10 +5,10 @@ Module for taking a mixed set of types and returning a tuple.
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Type-annotated function that takes a string an an int or float and
     returns a tuple. The first element of the tuple is a string and
     the second is the square of the int/float annotated as a float.
     """
-    return Tuple((k, float(v ** 2)))
+    return (k, v ** 2)
