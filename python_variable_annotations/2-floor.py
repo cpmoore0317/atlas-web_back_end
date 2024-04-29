624ec5ead53c1c2a93bcb32a88c542807b875810
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-04-29 12:41:00 -0500

    Testing simple return statement for 2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module for retrieving the the floor of a float.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Type-annotated function which takes a float
+    and returns the floor of the float.
+    """
+    return n
