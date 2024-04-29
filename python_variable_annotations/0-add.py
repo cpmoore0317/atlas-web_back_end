1cddd7e4d225c89c136679613f60ab8c2f9a8932
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-04-29 12:26:14 -0500

    Moving documentation underneath the function

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-"""
-add - Type-annotated function that adds two floats and
-returns their sum as a float.
-
-Parameters:
-a (float): The first number.
-b (float): The second number.
-
-Returns:
-The sum of the two numbers as a float.
-"""
 
 
 def add(a: float, b: float) -> float:
+    """
+    Type-annotated function that adds two floats and
+    returns their sum as a float.
+
+    Parameters:
+    a (float): The first number.
+    b (float): The second number.
+
+    Returns:
+    The sum of the two numbers as a float.
+    """
     return a + b
