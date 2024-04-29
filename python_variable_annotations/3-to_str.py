9aa83b4be00130e6b2be6d91755710b4dfacc8c9
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-04-29 12:49:38 -0500

    Returning the initial float n using the str function

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module for returning a float as a string.
+"""
+
+
+def to_str(n:float) -> str:
+    """
+    Type-annotated function that takes a float and
+    returns the string representation of it.
+    """
+    return n(str)
