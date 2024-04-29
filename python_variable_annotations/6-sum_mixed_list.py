896d01e49e74b27079f2e61e969a6204d42d78b5
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-04-29 13:24:39 -0500

    Returning a sum of mixed types using both types int and float in the list type annotation

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module for returning the sum of a list with mied types.
+"""
+from typing import List
+
+
+def sum_mixed_list(mxd_list: List[int, float]) -> float:
+    """
+    Type-annotated function which takes a list of integers and floats
+    and returns their sum as a float.
+    """
+    return sum(mxd_list)
