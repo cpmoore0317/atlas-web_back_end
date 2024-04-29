a09f11441445986248d1fe0b36090ad61029572d
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-04-29 13:09:52 -0500

    Iterating througha list of floats and returning the sum of all elements

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env
+"""
+Module for a function that takes a list of floats and returns their sum.
+"""
+from typing import List
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Type-annotated function which takes a list of floats
+    and returns their sum as a float.
+    """
+    for ele in range(0, len(input_list)):
+        total = total + input_list[ele]
+
+    return float(total)
