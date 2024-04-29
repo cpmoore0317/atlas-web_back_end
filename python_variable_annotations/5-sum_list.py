79307a9890c7a03a3511f78a1f107f1a2ed734d3
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-04-29 13:19:41 -0500

    Using sum function for sum_list return

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -10,7 +10,4 @@ def sum_list(input_list: List[float]) -> float:
     Type-annotated function which takes a list of floats
     and returns their sum as a float.
     """
-    for ele in range(0, len(input_list)):
-        total = total + input_list[ele]
-
-    return total
+    return sum(input_list)
