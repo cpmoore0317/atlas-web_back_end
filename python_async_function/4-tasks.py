f65dcbc42384b3052ceff93e1c6f8a3016237cad
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-04-30 13:00:55 -0500

    Fixing function definition syntax by adding a space before type annotation

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -6,7 +6,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n:int, max_delay: int) -> List[float]:
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Asynchronous coroutine that spawns n instances of task_wait_random with the
     specified max_delay. Returns a list of delays in ascending order.
