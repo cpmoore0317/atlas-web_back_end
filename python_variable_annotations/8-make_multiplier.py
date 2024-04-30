1bc7d8b8ec9ddda325682ddbf56104c85b07c488
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-04-29 22:25:16 -0500

    Using callable to return a function that returns a float

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -2,3 +2,15 @@
 """
 Module for returning a function that multiplies a float.
 """
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Type-annotated function that takes a multiplier float and returns a
+    function that multiplies that multiplier by another float.
+    """
+    def multiplier_function(x: float) -> float:
+        return x * multiplier
+    
+    return multiplier_function
