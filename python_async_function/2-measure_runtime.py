d790a5982862be5d1cbbcc497c1441f85cc02bef
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-04-30 12:38:19 -0500

    Measuring the time it takes for wait_n to execute with time.time() to calculate start and end time

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 """2-measure_runtime.py"""
+import asyncio
+import time
+wait_random = __import__('0-basic_async_syntax').wait_random
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measures the total execution time for wait_n
+    and returns average time per execution.
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
