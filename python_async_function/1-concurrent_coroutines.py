23323c20c6e19ceca94edd608903754abe3726e3
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-04-30 12:10:26 -0500

    Testing out sorted for getting the sorted list of coroutines to return

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Module that imports wait_random to create an async routine that takes 2 int arguments.
+wait_random spawns n times with the specified max_delay.
+"""
+from 0-basic_async_syntax import wait_random
+import asyncio
+from typing import List
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Asynchronously spawns n wait_random coroutines with the specifies max_delay
+    and returns a list of delays in ascending order.
+    """
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    results = await asyncio.gather(*tasks)
+    return sorted(results)
