90ba2ad159334061ec37e41c7cf0022b044ac5f4
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-01 16:33:04 -0500

    Using asyncio.get_event_loop to measure the total runtime

---
+++ python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""2-measure_runtime.py"""
+import asyncio
+from typing import List
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    Measures the total runtime of executing
+    async_comprehension four times in parallel.
+    """
+    start_time = asyncio.get_event_loop().time()
+
+    await asyncio.gather(
+        async_comprehension()
+        async_comprehension()
+        async_comprehension()
+        async_comprehension()
+    )
+
+    end_time = asyncio.get_event_loop().time()
+    total_runtime = end_time - start_time
+    return total_runtime
