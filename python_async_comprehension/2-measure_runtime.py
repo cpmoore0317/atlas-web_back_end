8e4640fa1f218a87eb1e315f5e5d6897e96afdaa
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-01 16:44:01 -0500

    Reverting to original usage of async.gather

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -2,16 +2,7 @@
 """2-measure_runtime.py"""
 import asyncio
 from typing import List
-async_generator = __import__('0-async_generator').async_generator
-
-
-async def async_comprehension() -> List[float]:
-    """
-    Collects 10 random numbers using an async
-    comprehension over async_generator.
-    """
-    result = [i async for i in async_generator()]
-    return result
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
