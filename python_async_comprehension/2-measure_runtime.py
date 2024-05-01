0a8a90d17a429ce539242575d208f87f4c146666
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-01 16:56:11 -0500

    Fixing closing bracket spacing for pycodestyle

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -16,7 +16,7 @@ async def measure_runtime() -> float:
                          async_comprehension(),
                          async_comprehension(),
                          async_comprehension()
-    )
+                        )
 
     end_time = asyncio.get_event_loop().time()
     total_runtime = end_time - start_time
