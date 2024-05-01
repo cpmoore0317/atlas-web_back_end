63fb2215a37339b11429d82a5888cc426bfd7054
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-01 16:37:19 -0500

    Testing out new import syntax

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -2,7 +2,7 @@
 """2-measure_runtime.py"""
 import asyncio
 from typing import List
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+__import__('1-async_comprehension')
 
 
 async def measure_runtime() -> float:
@@ -13,10 +13,10 @@ async def measure_runtime() -> float:
     start_time = asyncio.get_event_loop().time()
 
     await asyncio.gather(
-        async_comprehension
-        async_comprehension
-        async_comprehension
-        async_comprehension
+        async_comprehension()
+        async_comprehension()
+        async_comprehension()
+        async_comprehension()
     )
 
     end_time = asyncio.get_event_loop().time()
