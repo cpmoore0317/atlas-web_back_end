ac4433ece9e76c7c3379d3849712b5088ae4763d
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-01 16:48:21 -0500

    Adding commas in-between usages of async_comprehension within asyncio.gather function

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -13,9 +13,9 @@ async def measure_runtime() -> float:
     start_time = asyncio.get_event_loop().time()
 
     await asyncio.gather(
-        async_comprehension()
-        async_comprehension()
-        async_comprehension()
+        async_comprehension(),
+        async_comprehension(),
+        async_comprehension(),
         async_comprehension()
     )
 
