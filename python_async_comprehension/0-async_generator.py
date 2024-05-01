172bd4785adf91b42519b910f277db3b333aa771
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-01 16:12:48 -0500

    Removing in-line comment for type ignor

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]: # type: ignore
+async def async_generator() -> Generator[float, None, None]:
     """
     An asynchronous generator that yields
     random numbers between 0 and 10.
