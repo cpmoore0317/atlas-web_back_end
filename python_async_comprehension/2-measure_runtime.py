1d3674f0a7502c899c492b1e63f93063bd19454e
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-01 16:50:31 -0500

    Importing gather instead of using asyncio.gather

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """2-measure_runtime.py"""
 import asyncio
+from asyncio import gather
 from typing import List
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
@@ -12,7 +13,7 @@ async def measure_runtime() -> float:
     """
     start_time = asyncio.get_event_loop().time()
 
-    await asyncio.gather(
+    await gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
