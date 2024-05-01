ccb56c23f8109a428000565b40d13a647433afed
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-01 16:40:26 -0500

    Removing async_gather for running async_comprehension

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -12,12 +12,10 @@ async def measure_runtime() -> float:
     """
     start_time = asyncio.get_event_loop().time()
 
-    await asyncio.gather(
-        async_comprehension()
-        async_comprehension()
-        async_comprehension()
-        async_comprehension()
-    )
+    async_comprehension()
+    async_comprehension()
+    async_comprehension()
+    async_comprehension()
 
     end_time = asyncio.get_event_loop().time()
     total_runtime = end_time - start_time
