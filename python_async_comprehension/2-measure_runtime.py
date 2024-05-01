22135f385d0ad4c9ac461a8a7cbb75d1ee2de09c
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-01 16:35:38 -0500

    Testing out different syntax when using asyncio.gather

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -13,10 +13,10 @@ async def measure_runtime() -> float:
     start_time = asyncio.get_event_loop().time()
 
     await asyncio.gather(
-        async_comprehension()
-        async_comprehension()
-        async_comprehension()
-        async_comprehension()
+        async_comprehension
+        async_comprehension
+        async_comprehension
+        async_comprehension
     )
 
     end_time = asyncio.get_event_loop().time()
