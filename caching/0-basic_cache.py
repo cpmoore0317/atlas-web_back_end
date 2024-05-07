fca46225f7221deeb31f9a10f6669fc63956014f
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-07 12:59:41 -0500

    Basic cache

---
+++ caching/0-basic_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""0-basic_cache.py"""
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        """
+        Assigns the item value to the key in the cache.
+
+        Args:
+            key: The key for the cache entry.
+            item: The value to be stored in the cache.
+
+        Notes:
+            If key or item is None, this method does nothing.
+        """
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        Retrieves the value associated with the given key from the cache.
+
+        Args:
+            key: The key to look up in the cache.
+
+        Returns:
+            The value linked to the key, or None if the key doesn't exist.
+        """
+        return self.cache_data.get(key)
