9725a619ceb893729e00d98bf27e1434a44cb3f6
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-12 17:05:31 -0500

    Reverting LIFO file

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -25,17 +25,13 @@ class LIFOCache(BaseCaching):
             item: The value to be stored in the cache.
         """
         if key is not None and item is not None:
-            # If key already exists, update its value and move it to the end of the order
-            if key in self.cache_data:
-                self.key_order.remove(key)
             self.cache_data[key] = item
-            self.key_order.append(key)
-            
+            self.key_order.append(key)  # Add key to order
             if len(self.cache_data) > self.MAX_ITEMS:
-                # Pop the first added key (LIFO)
-                first_key = self.key_order.pop(0)
-                del self.cache_data[first_key]
-                print(f"DISCARD: {first_key}")
+                # Pop the last added key (LIFO)
+                last_key = self.key_order.pop(0)
+                del self.cache_data[last_key]
+                print(f"DISCARD: {last_key}")
 
     def get(self, key):
         """
