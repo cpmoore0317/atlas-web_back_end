72fde6b946ed534a265b18de3dc4210f54bb7cf1
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-12 17:04:54 -0500

    New put method for LIFO cache

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -25,13 +25,17 @@ class LIFOCache(BaseCaching):
             item: The value to be stored in the cache.
         """
         if key is not None and item is not None:
+            # If key already exists, update its value and move it to the end of the order
+            if key in self.cache_data:
+                self.key_order.remove(key)
             self.cache_data[key] = item
-            self.key_order.append(key)  # Add key to order
+            self.key_order.append(key)
+            
             if len(self.cache_data) > self.MAX_ITEMS:
-                # Pop the last added key (LIFO)
-                last_key = self.key_order.pop(0)
-                del self.cache_data[last_key]
-                print(f"DISCARD: {last_key}")
+                # Pop the first added key (LIFO)
+                first_key = self.key_order.pop(0)
+                del self.cache_data[first_key]
+                print(f"DISCARD: {first_key}")
 
     def get(self, key):
         """
