8a391811c0eba43fb66b6a8573ffe7e7ce430c70
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-12 17:16:39 -0500

    Pagination begins

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""0-simple_helper_function"""
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Returns a tuple containing the start and end indices for pagination.
+
+    Args:
+        page (int): The page number (1-indexed).
+        page_size (int): The number of items per page.
+
+    Returns:
+        tuple: A tuple containing the start and end indices.
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return start_index, end_index
