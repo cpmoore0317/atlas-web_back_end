acfa59eb7c6783cfd656b8a9ab51eacfbc112ba7
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-17 13:24:36 -0500

    Fixing filtered logger for output

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -9,13 +9,14 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
 
     Args:
         fields (List[str]): A list of strings representing fields to obfuscate.
-        redaction (str): A string representing the value to replace the fields with.
+        redaction (str): A string representing the value to replace the
+        fields with.
         message (str): A string representing the log line.
-        separator (str): A string representing the character separating fields in the log line.
+        separator (str): A string representing the character separating fields
+        in the log line.
 
     Returns:
         str: The obfuscated log message.
     """
-    for field in fields:
-        message = re.sub(f"{field}=[^;]+", f"{field}={redaction}", message)
-    return message
+    pattern = f"({'|'.join(fields)})=[^;]+"
+    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
