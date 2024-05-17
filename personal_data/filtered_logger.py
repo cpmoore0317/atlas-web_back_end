b5c2205c3f54ab25420363db87fa60d79d2962d7
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-17 13:32:33 -0500

    Removing whitespaces

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -8,13 +8,13 @@ def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     """
     Returns the log message with specified fields obfuscated.
-    
+
     Args:
         fields (List[str]): Fields to obfuscate.
         redaction (str): String to replace the field values.
         message (str): The log line to process.
         separator (str): Character that separates fields in the log line.
-    
+
     Returns:
         str: The obfuscated log message.
     """
