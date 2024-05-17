10fda5276be9dc3cfa3bfc1e77be98d4e4370564
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-17 13:31:19 -0500

    Filtered logger pycode fixes

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,7 +4,8 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """
     Returns the log message with specified fields obfuscated.
     
@@ -18,4 +19,5 @@ def filter_datum(fields: List[str], redaction: str, message: str, separator: str
         str: The obfuscated log message.
     """
     pattern = '|'.join(f"{field}=[^\\{separator}]*" for field in fields)
-    return re.sub(pattern, lambda m: m.group().split('=')[0] + '=' + redaction, message)
+    return re.sub(pattern, lambda m: m.group().split('=')[0] + '=' + redaction,
+                  message)
