504f79acabf9afb4b09440e4856b01ff4b066aeb
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-17 13:25:29 -0500

    Fixing pycodestyle

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,7 +3,9 @@
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """
     Obfuscates specified fields in a log message.
 
