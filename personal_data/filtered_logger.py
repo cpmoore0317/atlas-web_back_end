93a20a1420554d94caf5bc84e0531b4cb58623f2
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-17 13:19:57 -0500

    Type annotating filtered logger

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
 """filtered_logger.py"""
 import re
+from typing import List
 
-
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """
     Obfuscates specified fields in a log message.
 
     Args:
-        fields (list): A list of strings representing fields to obfuscate.
+        fields (List[str]): A list of strings representing fields to obfuscate.
         redaction (str): A string representing the value to replace the fields with.
         message (str): A string representing the log line.
         separator (str): A string representing the character separating fields in the log line.
