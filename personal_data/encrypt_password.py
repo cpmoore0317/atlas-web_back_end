a81518bd5b89ac31b16955396365c1ec13c88a29
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-17 20:15:01 -0500

    Using bcrypt library

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""encrypt_password.py"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hashes a password using bcrypt and returns the hashed password.
+
+    Args:
+        password (str): The password to hash.
+
+    Returns:
+        bytes: The salted, hashed password.
+    """
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode(), salt)
+    return hashed
+
+if __name__ == "__main__":
+    password = "MyAmazingPassw0rd"
+    print(hash_password(password))
+    print(hash_password(password))
