c974c83c4b34ec07c1dd6f27641151ef136e404f
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-05-17 20:17:09 -0500

    Updating encrpt_password.py task 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -17,8 +17,22 @@ def hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(password.encode(), salt)
     return hashed
 
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates that the provided password matches the hashed password.
+
+    Args:
+        hashed_password (bytes): The hashed password.
+        password (str): The plain text password to validate.
+
+    Returns:
+        bool: True if the password matches the hashed password, False otherwise.
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
+
 
 if __name__ == "__main__":
     password = "MyAmazingPassw0rd"
-    print(hash_password(password))
-    print(hash_password(password))
+    encrypted_password = hash_password(password)
+    print(encrypted_password)
+    print(is_valid(encrypted_password, password))
