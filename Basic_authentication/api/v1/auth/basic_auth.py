905e153213e54a187b0b8bbd1d23f84ee5c3a753
cpmoore0317 <cpmoore317@gmail.com>
2024-05-20 17:15:57 -0500

    Updating basic_auth.py for indentation syntax fix

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,18 +4,21 @@ Basic Auth module
 """
 from api.v1.auth.auth import Auth
 
+
 class BasicAuth(Auth):
     """
     BasicAuth class that inherits from Auth
     """
-    
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+
+    def extract_base64_authorization_header(self,
+    authorization_header: str) -> str:
         """
-        Extracts the Base64 part of the Authorization header for Basic Authentication.
-        
+        Extracts the Base64 part of the Authorization header for
+        Basic Authentication.
+
         Args:
             authorization_header (str): The Authorization header.
-        
+
         Returns:
             str: The Base64 part of the Authorization header or None.
         """
