dbbfa0add5e57576275ec9d3b011fd84bd9d4314
cpmoore0317 <cpmoore317@gmail.com>
2024-05-20 17:14:07 -0500

    Updating basic_auth.py

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -4,26 +4,28 @@ Basic Auth module
 """
 from api.v1.auth.auth import Auth
 
-
 class BasicAuth(Auth):
     """
     BasicAuth class that inherits from Auth
     """
-
+    
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
-    """
-    Extracts the Base64 part of the Authorization header for Basic Authentication.
+        """
+        Extracts the Base64 part of the Authorization header for Basic Authentication.
         
         Args:
             authorization_header (str): The Authorization header.
-
+        
         Returns:
             str: The Base64 part of the Authorization header or None.
-    """
-    if authorization_header is None:
-        return None
+        """
+        if authorization_header is None:
+            return None
 
-    if not isinstance(authorization_header, str):
-        return None
+        if not isinstance(authorization_header, str):
+            return None
 
-    if not authorization_header.split(" ", 1)[1]
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header.split(" ", 1)[1]
