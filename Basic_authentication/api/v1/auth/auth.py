602e80be71919232d5ce5f24b703a28b7f0dc359
cpmoore0317 <cpmoore317@gmail.com>
2024-05-20 16:38:49 -0500

    Updating require_auth function auth.py

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -30,7 +30,25 @@ class Auth:
         Returns:
         bool: False for now.
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        # Ensure the path ends with a slash
+        if non path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths"
+            if excluded_path.endswith('/'):
+                if path == excluded_path:
+
+            else:
+                if path.rstrip('/') == excluded_path.rstrip('/'):
+                    return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """
