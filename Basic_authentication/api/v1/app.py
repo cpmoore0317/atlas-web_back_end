45395b2d124708a7927d5d8db96c1abe9042d5da
cpmoore0317 <cpmoore317@gmail.com>
2024-05-20 17:00:17 -0500

    Fixing syntax error

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -16,7 +16,7 @@ auth = None
 
 AUTH_TYPE = getenv('AUTH_TYPE')
 if AUTH_TYPE == 'basic_auth':
-    from api.v1.auth.basic_auth impoty BasicAuth
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 elif AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
