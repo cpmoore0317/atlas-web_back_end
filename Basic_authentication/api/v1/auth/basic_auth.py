8d80d4436d580eed1dbae046b74ad02f983c7cbf
cpmoore0317 <cpmoore317@gmail.com>
2024-05-20 17:24:02 -0500

    Updated basic_auth.py with decode function

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -32,3 +32,23 @@ class BasicAuth(Auth):
             return None
 
         return authorization_header.split(" ", 1)[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+        Decodes a Base64 string.
+
+        Args:
+            base64_authorization_header (str): The Base64 string.
+
+        Returns:
+            str: The decoded value as UTF8 string or None if input is invalid.
+        """
+        if base64_authorization_header is None or not isinstance(base64_authorization_header, str):
+            return None
+
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            decoded_string = decoded_bytes.decode('utf-8')
+            return decoded_string
+        except:
+            return None
