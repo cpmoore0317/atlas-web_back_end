586e84f56b987ec46d23a333aed0624bf6160e99
cpmoore0317 <cpmoore317@gmail.com>
2024-05-20 21:45:55 -0500

    Updating decode base64 func

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 Basic Auth module
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -10,11 +11,9 @@ class BasicAuth(Auth):
     BasicAuth class that inherits from Auth
     """
 
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """
-        Extracts the Base64 part of the Authorization header for
-        Basic Authentication.
+        Extracts the Base64 part of the Authorization header for Basic Authentication.
 
         Args:
             authorization_header (str): The Authorization header.
