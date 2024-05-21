b9fbc8091741232d822b63b090074d448ea66c3f
cpmoore0317 <cpmoore317@gmail.com>
2024-05-20 21:52:14 -0500

    Trying to fix pycodestyle basic_auth.py

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,12 +10,10 @@ class BasicAuth(Auth):
     """
     BasicAuth class that inherits from Auth
     """
-
-    def extract_base64_authorization_header(self,
-                                            authorization_header: str) -> str:
+    
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """
-        Extracts the Base64 part of the Authorization header for
-        Basic Authentication.
+        Extracts the Base64 part of the Authorization header for Basic Authentication.
 
         Args:
             authorization_header (str): The Authorization header.
@@ -34,8 +32,7 @@ class BasicAuth(Auth):
 
         return authorization_header.split(" ", 1)[1]
 
-    def decode_base64_authorization_header(self, base64_authorization_header:
-                                           str) -> str:
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
         """
         Decodes a Base64 string.
 
