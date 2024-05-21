c8796b3ad944e23221c710ce9c65b24f6aa2e2e4
cpmoore0317 <cpmoore317@gmail.com>
2024-05-21 13:16:26 -0500

    Updating app.py

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -6,7 +6,6 @@ from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
-import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -53,14 +52,15 @@ def before_request():
         return
 
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
     if not auth.require_auth(request.path, excluded_paths):
         return
 
     if auth.authorization_header(request) is None:
         abort(401)
 
-    if auth.current_user(request) is None:
+    request.current_user = auth.current_user(request)
+    if request.current_user is None:
         abort(403)
 
 
