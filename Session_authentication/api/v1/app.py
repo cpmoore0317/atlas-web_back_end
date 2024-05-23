b25ae4328a5edfb715b4082fcf9c0a2638c57a6a
cpmoore0317 <cpmoore317@gmail.com>
2024-05-23 00:56:06 -0500

    Fixing pycodestyle for app.py

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -24,18 +24,20 @@ else:
     from api.v1.auth.auth import Auth
     auth = Auth()
 
+
 @app.before_request
 def before_request():
     """
     Before request handler to check for authentication.
     """
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/', '/api/v1/auth_session/login/']
-    
+
     if auth and auth.require_auth(request.path, excluded_paths):
         if not auth.authorization_header(request) and not auth.session_cookie(request):
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
+
 
 @app.errorhandler(401)
 def unauthorized(error):
@@ -44,12 +46,14 @@ def unauthorized(error):
     """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
 def forbidden(error):
     """
     Forbidden error handler.
     """
     return jsonify({"error": "Forbidden"}), 403
+
 
 @app.errorhandler(404)
 def not_found(error):
