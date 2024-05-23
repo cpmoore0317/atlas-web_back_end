98e03e1ab31701bfec1bf487bb8029cec0399a08
cpmoore0317 <cpmoore317@gmail.com>
2024-05-23 15:27:41 -0500

    Reverting changes

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -81,38 +81,3 @@ class SessionAuth(Auth):
 
         cookie_name = getenv("SESSION_NAME")
         return request.cookies.get(cookie_name)
-
-    session_auth = Blueprint('session_auth', __name__)
-
-    @session_auth.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-    def login():
-        """
-        POST /api/v1/auth_session/login
-        """
-        from api.v1.app import auth
-
-        email = request.form.get('email')
-        password = request.form.get('password')
-
-        if email is None or email == '':
-            return jsonify({"error": "email missing"}), 400
-
-        if password is None or password == '':
-            return jsonify({"error": "password missing"}), 400
-
-        user = User.search({"email": email})
-        if len(user) == 0:
-            return jsonify({"error": "no user found for this email"}), 404
-
-        user = user[0]
-        if not user.is_valid_password(password):
-            return jsonify({"error": "wrong password"}), 401
-
-        session_id = auth.create_session(user.id)
-        user_json = user.to_json()
-
-        response = jsonify(user_json)
-        session_name = getenv("SESSION_NAME")
-        response.set_cookie(session_name, session_id)
-
-        return response
