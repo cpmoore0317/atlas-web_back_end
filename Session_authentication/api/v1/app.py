bda583a143cbc613b24be9f84aa0af7559c404ca
cpmoore0317 <cpmoore317@gmail.com>
2024-05-23 01:00:43 -0500

    Breaking down require auth check to pass pycodestyle

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -34,8 +34,9 @@ def before_request():
                       '/api/v1/forbidden/', '/api/v1/auth_session/login/']
 
     if auth and auth.require_auth(request.path, excluded_paths):
-        if not auth.authorization_header(request) and not auth.session_cookie(
-            request):
+        auth_header = auth.authorization_header(request)
+        session_cookie = auth.session_cookie(request)
+        if not auth_header and not session_cookie:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
