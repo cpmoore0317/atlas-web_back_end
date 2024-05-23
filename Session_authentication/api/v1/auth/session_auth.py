2926505a84df0f39c7b5f21403dca3be5aab64b1
cpmoore0317 <cpmoore317@gmail.com>
2024-05-23 00:39:37 -0500

    Pycodestyle fixes

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -11,7 +11,8 @@ class SessionAuth(Auth):
     SessionAuth class for managing user sessions.
 
     Attributes:
-        user_id_by_session_id (dict): A dictionary to store session IDs and their corresponding user IDs.
+        user_id_by_session_id (dict): A dictionary to store session IDs and
+        their corresponding user IDs.
     """
 
     user_id_by_session_id = {}
@@ -28,7 +29,7 @@ class SessionAuth(Auth):
         """
         if user_id is None or not isinstance(user_id, str):
             return None
-        
+
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
