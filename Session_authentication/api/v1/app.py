b6f6e793c4bb5d8d895cb3726cd6bb1425919528
cpmoore0317 <cpmoore317@gmail.com>
2024-05-22 21:01:26 -0500

    Added session auth

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -7,6 +7,7 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -17,6 +18,9 @@ AUTH_TYPE = getenv('AUTH_TYPE')
 if AUTH_TYPE == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
+elif AUTH_TYPE == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 elif AUTH_TYPE == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
@@ -52,7 +56,7 @@ def before_request():
         return
 
     excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/', '/api/v1/auth_session/login/']
+                      '/api/v1/forbidden/']
     if not auth.require_auth(request.path, excluded_paths):
         return
 
---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,12 @@
+# api/v1/auth/session_auth.py
+"""
+Session Auth Module
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """
+    SessionAuth class for managing user sessions.
+    """
+    pass
