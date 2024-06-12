f8eb13a3e3e0e87e98fd77524eaad7e9d8fc37fd
cpmoore0317 <cpmoore317@gmail.com>
2024-06-12 16:42:26 -0500

    Fin

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,3 +2,10 @@
 """ A Python script that provides some stats about Nginx logs stored in MongoDB """
 
 
+def log_the_stats():
+    """Provides some stats about Nginx logs"""
+    pass
+
+
+if __name__ == "__main__":
+    log_the_stats()
