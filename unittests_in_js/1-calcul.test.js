55d86f5f7895a6b90c257e5d50c4110833de1eb5
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-06-17 15:53:17 -0500

    Task 1 v3

--- unittests_in_js/1-calcul.test.js
+++ unittests_in_js/1-calcul.test.js
@@ -5,7 +5,7 @@ const calculateNumber = require('./1-calcul.js');
 
 describe('calculateNumber', () => {
     describe('SUM', () => {
-        it('should add rounded numbers when type is SUM', () => {
+        it('should add rounded numbers when type is SUM', function() {
             assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
             assert.strictEqual(calculateNumber('SUM', 1, 3.7), 5);
             assert.strictEqual(calculateNumber('SUM', 1.2, 3), 4);
@@ -14,12 +14,12 @@ describe('calculateNumber', () => {
         });
     });
     describe('SUBTRACT', () => {
-        it('should subtract rounded numbers when type is SUBTRACT', () => {
+        it('should subtract rounded numbers when type is SUBTRACT', function() {
             assert.strictEqual(calculateNumber('SUBTRACT', 1, 3.7), -3);
         });
     });
     describe('DIVIDE', () => {
-        it('should divide rounded numbers when type is DIVIDE', () => {
+        it('should divide rounded numbers when type is DIVIDE', function() {
             assert.strictEqual(calculateNumber('DIVIDE', 1.2, 3.7), 0.324);
             assert.strictEqual(calculateNumber('DIVIDE', 1.5, 0), 'Error');
         });
