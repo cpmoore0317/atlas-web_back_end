a75dff0d745900e1b31f05b39e7b99b8143e917b
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-06-17 15:31:01 -0500

    Task 0 v5

--- unittests_in_js/0-calcul.test.js
+++ unittests_in_js/0-calcul.test.js
@@ -6,7 +6,8 @@ const calculateNumber = require('./0-calcul.js');
 describe('calculateNumber', () => {
     it('should return the sum of rounded numbers', () => {
         assert.strictEqual(calculateNumber(1, 3), 4);
-        assert.strictEqual(calculateNumber(1, 3.8), 5);
+        assert.strictEqual(calculateNumber(1, 3.7), 5);
+        assert.strictEqual(calculateNumber(1.2, 3), 4);
         assert.strictEqual(calculateNumber(1.2, 3.7), 5);
         assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     });
