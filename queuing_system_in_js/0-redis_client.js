fe9f12efea89aa12b3e59b3d7528334f3c0b2807
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-06-19 22:32:41 -0500

    Task 0 v1

---
+++ queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,15 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+    console.error('Redis client not connected to the server:', err.message);
+});
+
+client.connect().catch((err) => {
+    console.error('Redis client not connected to the server:', err.message);
+});
