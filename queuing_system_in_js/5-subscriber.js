6a561a3c5c9da0bf8f590df73cd3252fbace3179
cpmoore0317 <126629203+cpmoore0317@users.noreply.github.com>
2024-06-19 22:44:38 -0500

    Task 5 v4

---
+++ queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,23 @@
+import { createClient } from 'redis';
+
+const subscriber = createClient();
+
+subscriber.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+subscriber.on('error', (err) => {
+    console.error('Redis client not connected to the server:', err.message);
+});
+
+subscriber.connect().catch((err) => {
+    console.error('Redis client not connected to the server:', err.message);
+});
+
+subscriber.subscribe('holberton school channel', (message) => {
+    console.log(message);
+    if (message === 'KILL_SERVER') {
+        subscriber.unsubscribe('holberton school channel');
+        subscriber.quit();
+    }
+});
